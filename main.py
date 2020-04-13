59ebf8074a3e41261c8684ff2c997ded2822818c
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-13 18:16:23 +0300

    solved: 36ms (~35%) / 14.1 (5%) with additional calculations

--- main.py
+++ main.py
@@ -1,43 +1,56 @@
 from typing import List
 
-class Solution:
-  def convertListToStr(self, items: List[str]):
-      sortedList = list(set(items))
-      sortedList.sort()
-      return '/'.join([item for item in sortedList])
 
-  def findBFS(self, nums: List[str], path: List[str], ans: List[List[str]] ):
+class Solution:
+
+  def convertListToSortKeyStr(self, items: List[str]):
+    sortedList = list(set(items))
+    sortedList.sort()
+    return '/'.join([item for item in sortedList])
+
+  def findBFS(self, nums: List[str], path: List[str], arrAns: List[List[str]],
+              dictAns: dict):
 
     for i in range(len(nums)):
-      newNums = nums[:i] + nums[i+1:]
+      newNums = nums[:i] + nums[i + 1:]
       newPath = path + [nums[i]]
-      print(newPath)
-      ans.append(newPath)
-      self.findBFS(newNums, newPath, ans)
 
+      arrAns.append(newPath)
+      keyStr = self.convertListToSortKeyStr(newPath)
 
+      if keyStr not in dictAns:
+        self.calcCount += 1
+        dictAns[keyStr] = newPath
+        self.findBFS(newNums, newPath, arrAns, dictAns)
+      else:
+        self.breakCount += 1
 
   def subsets(self, nums: List[int]) -> List[List[str]]:
-    ans = []
+    arrAns = []
+    dictAns = {}
+    self.calcCount = 0
+    self.breakCount = 0
     strNums = [str(item) for item in nums]
-    print(strNums)
-    self.findBFS(strNums, [], ans)
+    self.findBFS(strNums, [], arrAns, dictAns)
 
+    arrAns = list(dictAns.values())
 
+    print('Calc! %d' % self.calcCount)
+    print('BREAK! %d' % self.breakCount)
 
-    newAns = list(set([self.convertListToStr(itemList) for itemList in ans]))
-    print(newAns)
-
-    ans = [[int(innerItem) for innerItem in item.split('/')] for item in newAns]
-    ans.append([])
-    print(ans)
-
-    return ans
+    return list(dictAns.values()) + [[]]
 
 
 my = Solution()
-n = [1,2,3]
+n = [1, 2, 3]
 # TODO: TOO SLOW !!!
-# n = [1,2,3,4,5,6,7,8,10,0]
+# n = [1, 2, 3, 4, 5, 6, 7, 8, 10, 0]
 ans = my.subsets(n)
-# print("ans", ans)
+print("ans", ans)
+
+# 1[2,3] - 1,2[3] - 1,2,3[]
+# [[1], [1,2], [1,2,3]]
+# 2[1,3] - 2,1[3] -> break!
+# [[1], [1,2], [1,2,3], [2]]
+# 3[1,2] - 3,1[2] -> break!
+# [[1], [1,2], [1,2,3], [2], [3], [3,1]]
